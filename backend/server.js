edfaf1ae9878c2cf214d132a519416cceffb8fcb
Bastianmoral <basti@gmail.com>
2023-07-19 15:01:45 -0400

    adding mongoose and mongodb conection

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const apiRoutes = require("./routes/apiRoutes")
+
+app.get('/', (req, res) => {
+    res.json({message: "API running..."})
+})
+
+// MONGODB CONNECTION 
+const connectDB = require("./config/db")
+connectDB();
+
+
+app.use('/api', apiRoutes)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
