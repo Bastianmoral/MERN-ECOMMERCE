746689a25cf44ce512dc1c673035a8c2dfe5b387
Bastianmoral <bastihc@gmail.com>
2023-11-29 00:08:06 -0300

    adding seeder product to mongodb

--- backend/Seeder/seeder.js
+++ backend/Seeder/seeder.js
@@ -4,13 +4,21 @@ connectDB()
 
 
 const categoryData = require("./categories")
+const productData = require("./products")
 const Category = require("../models/categoryModel")
+const Product = require("../models/ProductModel")
+
 
 const importData = async () =>  {
     try {
-        //await Category.collection.dropIndexes() asi se borran los indices que ya estén agregados. 
+        await Category.collection.dropIndexes() //asi se borran los indices que ya estén agregados. 
+        await Product.collection.dropIndexes() //asi se borran los indices que ya estén agregados. 
+        
         await Category.collection.deleteMany({})
+        await Product.collection.deleteMany({})
+        
         await Category.insertMany(categoryData)
+        await Product.insertMany(productData)
         console.log("Seeder data proceeded successfully")
         process.exit()
     } catch(error) {    
