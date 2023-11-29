c2501b7555b08ccb5b8b24b0454825d8ebbe7243
Bastianmoral <bastihc@gmail.com>
2023-11-29 00:33:21 -0300

    adding review seeder

--- backend/Seeder/seeder.js
+++ backend/Seeder/seeder.js
@@ -5,20 +5,27 @@ connectDB()
 
 const categoryData = require("./categories")
 const productData = require("./products")
+const reviewData = require("./reviews")
+
+
 const Category = require("../models/categoryModel")
 const Product = require("../models/ProductModel")
+const Review= require("../models/reviewsModel")
 
 
 const importData = async () =>  {
     try {
         await Category.collection.dropIndexes() //asi se borran los indices que ya estén agregados. 
         await Product.collection.dropIndexes() //asi se borran los indices que ya estén agregados. 
+       
         
         await Category.collection.deleteMany({})
         await Product.collection.deleteMany({})
+        await Review.collection.deleteMany({})
         
         await Category.insertMany(categoryData)
         await Product.insertMany(productData)
+        await Review.insertMany(reviewData)
         console.log("Seeder data proceeded successfully")
         process.exit()
     } catch(error) {    
